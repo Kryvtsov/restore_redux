dd67bea795cafddc9e9d34413b2a16acae096f25
Vladymyr <vladymyr13@gmail.com>
2019-01-22 00:01:30 +0200

    route and lists components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,22 @@
-import React, { Component } from 'react';
-import {withBookstoreService} from '../hoc'
+import React, {Component} from 'react';
+import { Route, Switch } from 'react-router-dom';
+import {HomePage, CartPage} from "../pages";
+import './style.css';
 
-import './style.css'
-const App = ({bookstoreService}) => {
-
-    console.log(bookstoreService.getBooks())
-        return(
-            <div>
-                Hello
-            </div>
-        )
+const App = () => {
+    return (
+        <Switch>
+            <Route
+                path="/"
+                component={HomePage}
+                exact/>
+            <Route
+                path="/cart"
+                component={CartPage}
+                />
+        </Switch>
+    )
 
 }
 
-export default withBookstoreService()(App)
+export default App;
---
+++ src/components/pages/home-page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BookList from "../book-list";
+const HomePage = () => {
+    return (
+        <BookList books={[{id: 1, title: 'dcdkcm', author: 'sdecerc' }]}/>
+    )
+
+};
+
+export default HomePage;
